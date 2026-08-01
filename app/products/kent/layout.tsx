18f3c307e80832c341763b98dcc9e3852a3b7e66
haedalho <limroq2@gmail.com>
2026-08-02 01:17:42 +0900

    Remove Kent image recovery from layout

--- app/products/kent/layout.tsx
+++ app/products/kent/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
 
-import KentImageRecoveryClient from "@/components/products/KentImageRecoveryClient";
-
 export default function KentLayout({ children }: { children: ReactNode }) {
-  return (
-    <div data-kent-image-recovery-root>
-      <KentImageRecoveryClient />
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
