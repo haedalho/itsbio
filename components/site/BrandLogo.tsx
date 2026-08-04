ab760867a0ece2e9c8f7828558c73a059c4f6ed5
haedalho <limroq2@gmail.com>
2026-08-05 01:24:41 +0900

    Render scalable ITSBIO logo with Next Image

--- components/site/BrandLogo.tsx
+++ components/site/BrandLogo.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function BrandLogo({ className = "" }: { className?: string }) {
@@ -7,11 +8,13 @@ export default function BrandLogo({ className = "" }: { className?: string }) {
       aria-label="ITS BIO home"
       className={`inline-flex h-12 w-[142px] items-center ${className}`}
     >
-      <img
+      <Image
         src="/images/brand/itsbio-logo.svg"
         alt="ITSBIO"
         width={505}
         height={195}
+        priority
+        unoptimized
         className="block h-auto w-[132px]"
       />
     </Link>
