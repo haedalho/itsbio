c1adeba10175d163e5c53bcc51b4f9a3d6f70376
haedalho <limroq2@gmail.com>
2026-08-06 15:42:54 +0900

    Redirect legacy SomnoSuite accessories path

---
+++ app/products/kent/anesthesia/anesthesia-accessories-for-somnosuite/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function LegacySomnoSuiteAccessoriesPage() {
+  redirect("/products/kent/anesthesia/anesthesia-accessories/anesthesia-accessories-for-somnosuite");
+}
