031441d4df31de2ba202f270544b94e1a5630068
haedalho <limroq2@gmail.com>
2026-08-06 15:43:02 +0900

    Redirect legacy VetFlo accessories path

---
+++ app/products/kent/anesthesia/anesthesia-accessories-for-vetflo/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function LegacyVetFloAccessoriesPage() {
+  redirect("/products/kent/anesthesia/anesthesia-accessories/anesthesia-accessories-for-vetflo");
+}
