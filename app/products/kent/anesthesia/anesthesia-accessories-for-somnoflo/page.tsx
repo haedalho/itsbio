426a99227324291353cd35aeaa6cb09ff4d300a6
haedalho <limroq2@gmail.com>
2026-08-15 20:15:43 +0900

    Fix Kent accessory category redirect loops

--- app/products/kent/anesthesia/anesthesia-accessories-for-somnoflo/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function LegacySomnoFloAccessoriesPage() {
-  redirect("/products/kent/anesthesia/anesthesia-accessories/anesthesia-accessories-for-somnoflo");
-}
--- app/products/kent/anesthesia/anesthesia-accessories-for-vetflo/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function LegacyVetFloAccessoriesPage() {
-  redirect("/products/kent/anesthesia/anesthesia-accessories/anesthesia-accessories-for-vetflo");
-}
