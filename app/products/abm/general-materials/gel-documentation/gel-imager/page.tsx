63d85f8f56d929b19eab245baec692dce9099d60
haedalho <limroq2@gmail.com>
2026-08-17 09:50:04 +0900

    Fix ABM Gel Imager category page

---
+++ app/products/abm/general-materials/gel-documentation/gel-imager/page.tsx
@@ -0,0 +1,5 @@
+import AbmGelDocumentationLanding from "@/components/products/AbmGelDocumentationLanding";
+
+export default function GelImagerPage() {
+  return <AbmGelDocumentationLanding kind="gel-imager" />;
+}
