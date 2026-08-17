89d16e1a9bbd1c8c7447cd036f9706595aeb21b6
haedalho <limroq2@gmail.com>
2026-08-17 09:49:54 +0900

    Fix ABM DNA Stains category page

---
+++ app/products/abm/general-materials/gel-documentation/dna-stains/page.tsx
@@ -0,0 +1,5 @@
+import AbmGelDocumentationLanding from "@/components/products/AbmGelDocumentationLanding";
+
+export default function DnaStainsPage() {
+  return <AbmGelDocumentationLanding kind="dna-stains" />;
+}
