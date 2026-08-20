877bc0014e39682958371f63828cf0edc480ab53
haedalho <limroq2@gmail.com>
2026-08-20 15:27:44 +0900

    Keep Stem Cell-Derived parent route explicit

---
+++ app/products/abm/cellular-materials/cell-library-collections/stem-cell-derived-cells/page.tsx
@@ -0,0 +1,5 @@
+import CellLibraryDetailPage from "../[slug]/page";
+
+export default function StemCellDerivedCellsPage() {
+  return CellLibraryDetailPage({ params: { slug: "stem-cell-derived-cells" } });
+}
